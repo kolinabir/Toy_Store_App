21dd92910d8fdfed5b0ac404f0f8ab796117368e
kolinabir <knkolin9@gmail.com>
2023-05-19 23:36:22 +0600

    toy section added

--- src/Components/FirstPage/ShopByCat.jsx
+++ src/Components/FirstPage/ShopByCat.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import ToysCategoryCard from "./ToysCategoryCard";
 
 const ShopByCat = () => {
+  const categoryNames = ["Speed", "Retro", "Off-Road"];
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center mx-3">
       <Tabs>
-        <TabList>
+        <TabList className="flex justify-center text-sm  lg:text-2xl">
           <Tab>Speed Demons</Tab>
           <Tab>Off-Road Explorers</Tab>
           <Tab>Retro Classics</Tab>
         </TabList>
-
+        <hr />
+        <br />
         <TabPanel>
-          <h2>Any content 1</h2>
+          <ToysCategoryCard categoryName={categoryNames[0]}></ToysCategoryCard>
         </TabPanel>
         <TabPanel>
-          <h2>Any content 2</h2>
+          <ToysCategoryCard categoryName={categoryNames[1]}></ToysCategoryCard>
         </TabPanel>
         <TabPanel>
-          <h2>Any content 3</h2>
+          <ToysCategoryCard categoryName={categoryNames[2]}></ToysCategoryCard>
         </TabPanel>
       </Tabs>
     </div>
---
+++ src/Components/FirstPage/ToysCategoryCard.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+
+const ToysCategoryCard = ({categoryName}) => {
+  const [toys, setToys] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5003/toys/${categoryName}`) // Make a GET request to the server with the activeTab value in the URL
+      .then((res) => res.json()) // Convert the response to JSON format
+      .then((result) => {
+        console.log(result);
+        setToys(result); // Update the job state with the fetched result
+      });
+  }, []);
+
+  return (
+    <div className="grid lg:grid-cols-4 gap-3">
+      {toys.map((toy) => (
+        <div className="">
+          <div className="relative flex flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+            <div className="relative mx-4 mt-4 h-96 overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700">
+              <img
+                src={toy.pictureURL}
+                className="h-full w-full object-cover "
+                alt="Product"
+              />
+            </div>
+            <div className="p-6">
+              <div className="mb-2 flex items-center justify-between">
+                <p className="text-blue-gray-900 block font-sans text-2xl font-bold leading-relaxed antialiased">
+                  {toy.name}
+                </p>
+                
+                <p className="text-blue-gray-900 block font-sans text-base font-medium leading-relaxed antialiased">
+                  ${toy.price}
+                </p>
+              </div>
+              <h2>Rating: {toy.rating}</h2>
+              <p>{toy.description}</p>
+            </div>
+            <div className="p-6 pt-0">
+              <button
+                className="btn btn-accent"
+                type="button"
+              >
+                View Details
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ToysCategoryCard;
